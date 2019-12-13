91fedb683cc14d0a850ef22a4341aae110e5d749
Haroon Rafique <haroon.rafique@protonmail.com>
2019-12-13 12:26:00 +0100

    04: SLURM Q changed

--- 04_Mini_Scan/00_Master/Make_SLURM_submission_script.py
+++ 04_Mini_Scan/00_Master/Make_SLURM_submission_script.py
@@ -19,7 +19,7 @@ clean_all = True		# Clean simulation folder before running (False when resuming 
 # Must be chosen
 
 # ~ queue = 'inf-long', 'inf-short', 'batch-long', 'batch-short'
-queue = 'batch-short'
+queue = 'batch-long'
 
 n_nodes = 2 
 
