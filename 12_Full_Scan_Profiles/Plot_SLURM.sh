1118ba430b522837ab8857d919638e41a41ec7b5
Haroon Rafique <haroon.rafique@protonmail.com>
2020-01-20 09:22:34 +0100

    12: Plot_SLURM.sh script added to submit plots on HPC-Batch

--- 12_Full_Scan_Profiles/Plot_SLURM.sh
+++ 12_Full_Scan_Profiles/Plot_SLURM.sh
@@ -42,12 +42,13 @@ module load mpi/mvapich2/2.3
 tstart=$(date +%s)
 
 # Run the job
-srun --exclusive -n 1 ./01_Op_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
-srun --exclusive -n 1 ./01_ReM_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
-srun --exclusive -n 1 ./01_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
-srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
-srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
-srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py
+srun --exclusive -n 1 ./01_Op_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+srun --exclusive -n 1 ./01_ReM_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+srun --exclusive -n 1 ./01_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+srun --exclusive -n 1 ./02_Lattice_NoSC_NoRF/Plot_Tune_and_Distn_Footprints.py &
+wait
 
 tend=$(date +%s)
 dt=$(($tend - $tstart))
